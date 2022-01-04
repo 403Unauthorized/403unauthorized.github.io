532e18b8e8cb1c9ed912b51353f88f25a75c1251
torres.lei <torres.lei@rakuten.com>
2022-01-04 18:43:50 +0900

    updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 # Push HTML files to gh-pages automatically.
 
 # Fill this out with the correct org/repo
-ORG=24OI
-REPO=OI-wiki
+AUTHOR=Torres.Lei
+REPO=403unauthorized.github.io
 # This probably should match an email for one of your users.
-EMAIL=sirius.caffrey@gmail.com
+EMAIL=leiyongqi1026@gmail.com
 INSTALL_THEME='./scripts/install_theme.sh'
 
 set -e
