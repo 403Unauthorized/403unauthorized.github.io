0de7361bb743345afbc9785f4ab6ac6c7ea4e8ae
torres.lei <torres.lei@rakuten.com>
2022-01-04 17:21:10 +0900

    updates

--- scripts/install_theme.sh
+++ scripts/install_theme.sh
@@ -6,5 +6,3 @@ THEME_NAME="mkdocs-material"
 if [ ! -d "$THEME_NAME" ] ; then
   git clone --depth=1 ${THEME_REPO:-"https://github.com/OI-wiki/mkdocs-material.git"} "$THEME_NAME"
 fi
-
-bash scripts/install_theme_vendor.sh
