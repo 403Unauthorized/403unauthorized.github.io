a9750fa5c9306048267ae0557a7209c1fcda49d8
torres.lei <torres.lei@rakuten.com>
2022-01-06 17:17:51 +0900

    mkdocs-material url change

--- scripts/install_theme.sh
+++ scripts/install_theme.sh
@@ -4,5 +4,5 @@ set -e
 
 THEME_NAME="mkdocs-material"
 if [ ! -d "$THEME_NAME" ] ; then
-  git clone --depth=1 ${THEME_REPO:-"https://github.com/OI-wiki/mkdocs-material.git"} "$THEME_NAME"
+  git clone --depth=1 ${THEME_REPO:-"https://github.com/squidfunk/mkdocs-material.git"} "$THEME_NAME"
 fi
